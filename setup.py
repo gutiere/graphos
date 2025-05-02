6040bd0dd229f952cef70a30ebd06bd26e4f2bd8
Elijah Gutierrez <EdgardoGutierrezJr@Gmail.com>
2025-05-01 22:01:18 -0700

    Update version, deployed 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graphos",
-    version="0.2.2",
+    version="0.2.3",
     author="Edgardo Gutierrez Jr.",
     author_email="edgardogutierrezjr@gmail.com",
     description="Terminal graph visualization tool.",
